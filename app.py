07481ffb33aa3f66f214799cac74782996ef2585
varunteja123456-lgtm <varunteja123456@gmail.com>
2026-05-16 16:12:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pandas as pd
 scope = ["https://www.googleapis.com/auth/spreadsheets", "https://www.googleapis.com/auth/drive"]
 creds = Credentials.from_service_account_info(st.secrets["gcp_service_account"], scopes=scope)
 client = gspread.authorize(creds)
-SHEET_NAME = "Varun’s Cinematic Buffet"
+SHEET_NAME = "Varun’s Personal Buffet"
 spreadsheet = client.open(SHEET_NAME)
 sheet = spreadsheet.sheet1
 
